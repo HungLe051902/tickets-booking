ce5321740d0b1078b417d39d87599d6abb22bdd7
HungLe <lexuanhung020599@gmail.com>
2025-08-21 01:04:46 +0700

    Add docker compose for kafka and BookingService

---
+++ BookingService/Models/Booking.cs
@@ -0,0 +1,9 @@
+﻿namespace BookingService.Models
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public int ShowId { get; set; }
+        public int SeatNumber { get; set; }
+    }
+}
